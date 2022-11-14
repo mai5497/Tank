4fee7901d8d8ad83c0051382ecbf776edd97b7f9
itida <maimai130806@gmail.com>
2022-11-15 00:45:02 +0900

    エネミーをクラス化中

---
+++ DX11Game/EnemyManager.h
@@ -0,0 +1,15 @@
+#ifndef __ENEMYMANAGER_H__
+#define __ENEMYMANAGER_H__
+
+class EnemyManager {
+public:
+	EnemyManager();
+	~EnemyManager();
+
+	void Init();
+	void Uninit();
+	void Update();
+	void Draw();
+};
+ 
+#endif // __ENEMYMANAGER_H__
